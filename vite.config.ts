ecec92ad2004f4136da6080e7d3ba1bc74b4f8cd
KutsunaSubaRya <index20010928@gmail.com>
2023-04-30 23:07:08 +0800

    update VITE_PUBLIC_PATH

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import { resolve } from "node:path";
 import vue from "@vitejs/plugin-vue"
 import Icons from "unplugin-icons/vite"
@@ -9,51 +9,54 @@ import AutoImport from "unplugin-auto-import/vite"
 import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: env.VITE_PUBLIC_PATH,
-  plugins: [
-    vue(),
-    Components({
-        resolvers: [
-            IconsResolver({
-                prefix: "i",
-            }),
-            PrimeVueResolver(),
-            
+export default defineConfig(({ command, mode }) =>{
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: env.VITE_PUBLIC_PATH,
+    plugins: [
+      vue(),
+      Components({
+          resolvers: [
+              IconsResolver({
+                  prefix: "i",
+              }),
+              PrimeVueResolver(),
+              
+          ],
+          dts: true,
+          include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      }),
+      Icons({ autoInstall: true }),
+      AutoImport({ 
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/, /\.vue\?vue/, // .vue
+          /\.md$/, // .md
         ],
-        dts: true,
-        include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-    }),
-    Icons({ autoInstall: true }),
-    AutoImport({ 
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/, /\.vue\?vue/, // .vue
-        /\.md$/, // .md
-      ],
-      imports: [
-        // presets
-        "vue",
-        "vue-router",
-      ],
-      eslintrc: {
-				enabled: false, // Default `false`
-				filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
-				globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
-			},
-    }),
-    WindiCSS(),
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
+        imports: [
+          // presets
+          "vue",
+          "vue-router",
+        ],
+        eslintrc: {
+          enabled: false, // Default `false`
+          filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+          globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+        },
+      }),
+      WindiCSS(),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
-  // server: {
-  //   port: 4000,
-  //   hmr: {
-  //       host: "127.0.0.1",
-  //       port: 4000
-  //   },
-  // }
-})
+    // server: {
+    //   port: 4000,
+    //   hmr: {
+    //       host: "127.0.0.1",
+    //       port: 4000
+    //   },
+    // }
+    
+}})
