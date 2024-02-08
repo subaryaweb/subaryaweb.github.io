2dda5d684c78cf38e421a74770df0e478daff3fe
KutsunaSubaRya <index20010928@gmail.com>
2024-02-08 12:42:51 +0800

    feat: add about page card content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,18 @@
 <template>
+	<Content class="h-[calc(100vh-72px)] overflow-y-auto py-8 px-12 flex items-center justify-center flex-col">
+		<t-card hover-shadow header-bordered
+				class="flex items-center justify-center font-bold text-3xl w-2/3 h-2/3 rounded-3xl px-24">
+			<div class="text-3xl mb-4 text-center">Hi 👋, I'm SubaRya - 蘇子權</div>
+			<div class="text-xl mb-4 text-center">A CSIE student from National Taiwan Normal University (NTNU).</div>
+
+			<div class="text-xl">Love develop project to solve problems. Additionally, I enjoy self researching and sharing my findings !</div>
+		</t-card>
+	</Content>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Card, Content } from "tdesign-vue-next";
+</script>
 
 <style lang="scss">
 </style>
